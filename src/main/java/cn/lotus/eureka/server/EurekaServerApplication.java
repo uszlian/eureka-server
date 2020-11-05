d0ae845ce676f1e008b2e9f6889cc2cc36c6d637
史锦波 <shijinbo@hpcang.com>
2020-11-05 22:48:58 +0800

    初始化

---
+++ src/main/java/cn/lotus/eureka/server/EurekaServerApplication.java
@@ -0,0 +1,18 @@
+package cn.lotus.eureka.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author shijinbo
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+}
