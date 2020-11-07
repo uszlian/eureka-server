6442a584725a734f79822236510038d144eb5da1
史锦波 <shijinbo@hpcang.com>
2020-11-07 10:23:58 +0800

    eureka 注册中心服务

--- src/main/java/cn/lotus/server/eureka/EurekaServerApplication.java
+++ src/main/java/cn/lotus/server/eureka/EurekaServerApplication.java
@@ -1,4 +1,4 @@
-package cn.lotus.eureka.server;
+package cn.lotus.server.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
